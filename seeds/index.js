4cbe0634836cf49308db76b738bda08be2fc6523
Pablo <tackpablo@gmail.com>
2021-11-21 16:03:06 -0800

    Authorization

--- seeds/index.js
+++ seeds/index.js
@@ -22,6 +22,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '619a8f0fb036469cc0f02715',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
