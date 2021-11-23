cdf664d00956043cc361c15bc5e71c79b47f12b6
Pablo <tackpablo@gmail.com>
2021-11-22 22:25:43 -0800

    Map addition

--- seeds/index.js
+++ seeds/index.js
@@ -22,11 +22,16 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            //YOUR USER ID
             author: '619a8f0fb036469cc0f02715',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi mollitia quibusdam quasi modi atque quo aspernatur earum vitae blanditiis, excepturi placeat molestias corporis ipsam, aliquam accusantium obcaecati suscipit tempore doloremque?',
             price,
+            geometry: {
+                type: "Point", 
+                coordinates: [-113.1331, 47.0202]
+            },
             images: [
                 {
                     url: 'https://res.cloudinary.com/tackpablo/image/upload/v1637610153/YelpCamp/gmd0kuahsh5tdacth5la.jpg',
