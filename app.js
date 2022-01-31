9353617b45ae35c7b12e878ad941846885b905c7
Pablo Tack <93554557+tackpablo@users.noreply.github.com>
2022-01-31 13:12:32 -0800

    Update app.js

--- app.js
+++ app.js
@@ -94,12 +94,11 @@ const styleSrcUrls = [
     "https://use.fontawesome.com/",
     "https://cdn.jsdelivr.net",
 ];
-
 const connectSrcUrls = [
-    "https://api.mapbox.com",
-    "https://*.tiles.mapbox.com",
-    "https://events.mapbox.com",
-    "https://ka-f.fontawesome.com"
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
 ];
 
 const fontSrcUrls = [];
