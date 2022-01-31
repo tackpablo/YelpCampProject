d40969755a3e6cdfe784bdf51c6f49a5d90b8403
Pablo Tack <93554557+tackpablo@users.noreply.github.com>
2022-01-31 13:08:23 -0800

    Update app.js

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.use(express.static(__dirname+"/public"));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(mongoSanitize());
 
 const secret = process.env.SECRET || 'thisshouldbeabettersecret'
